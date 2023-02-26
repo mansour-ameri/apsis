09d1e7c369fc0040a3c3bc8af9510a8200174bfa
mansour-ameri <mansour.ameri@gmail.com>
2023-02-26 23:02:59 +0100

    init commit

---
+++ src/main/java/com/apsis/apsis/ApsisApplication.java
@@ -0,0 +1,13 @@
+package com.apsis.apsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apsis/apsis/ApsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsis.apsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
